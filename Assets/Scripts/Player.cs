061596631aea757749d6c9b25fde6bb456a6b1a3
Ashutosh <ashunandanwar98@gmail.com>
2024-06-02 13:44:21 +0530

    Player Controller and Setting Up a player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float _playerSpeed = 7f;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * _playerSpeed * Time.deltaTime;
+
+    }
+}
