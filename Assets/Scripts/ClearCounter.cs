f985e272d08141f40decc5f56430b62eef02e687
Ashutosh <ashunandanwar98@gmail.com>
2024-06-13 20:18:19 +0530

    Scriptable Object

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
     [SerializeField]
-    private Transform tomatoPrefab;
+    private KitchenObjectSO kitchenObjectSO;
     [SerializeField]
     private Transform counterTopPoint;
+
+    private KitchenObject kitchenObject;
     public void Interact()
     {
-        Debug.Log("Interact");
-        Transform tomatoTransform = Instantiate(tomatoPrefab,counterTopPoint);
-        tomatoTransform.localPosition = Vector3.zero;
+        if(kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.localPosition = Vector3.zero;
+
+            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            kitchenObject.SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+
+       
     }
 }
---
+++ Assets/Scripts/KitchenObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenObject : MonoBehaviour
+{
+    [SerializeField]
+    private KitchenObjectSO kitchenObjectSO;
+
+    private ClearCounter clearCounter;
+    public KitchenObjectSO GetKitchenObject()
+    {
+        return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        this.clearCounter = clearCounter;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
+    }
+}
